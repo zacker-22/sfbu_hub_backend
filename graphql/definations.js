db447ed5c46db7fb115d7e3a32eb3ce15da2c6a9
Yash Shah <zackeryy22@gmail.com>
2024-02-03 22:04:09 -0800

    init commit

---
+++ graphql/definations.js
@@ -0,0 +1,51 @@
+export const typeDefs = `#graphql
+    # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+    scalar DateTime
+
+    type LoginResponse {
+        error: Boolean,
+        error_message: String,
+        error_code: Int,
+        token: String
+    }
+
+    type Query {
+        otpRequest(email: String!): LoginResponse
+        login(email: String!, otp: String!): LoginResponse
+    }
+
+    
+`;
+
+
+export const resolvers = {
+    Query: {
+        otpRequest: async (parent, args, context, info) => {
+            const database = context.database;
+            const collection = database.collection('users');
+            const user = await collection.findOne({email: args.email});
+            if(!user){
+                collection.insertOne({email: args.email, otp: "1234", timestamp: new Date()});
+            }
+            else{
+                collection.updateOne({email: args.email}, {$set: {otp: "1234", timestamp: new Date()}});
+            }
+            return {error: false, error_message: "OTP sent to your email"};
+        },
+        login: async (parent, args, context, info) => {
+            const database = context.database;
+            const collection = database.collection('users');
+            const user = await collection.findOne({email: args.email, otp: args.otp});
+            await collection.updateOne({email: args.email}, {$set: {otp: "", token: "asdfghjkl", timestamp: new Date()}} );
+
+            if(user){
+                return {error: false, error_message: "Logged in successfully", token: "asdfghjkl"};
+            }
+            else{
+                return {error: true, error_message: "Invalid OTP"};
+            }
+        }
+
+    },
+    
+};
---
+++ index.js
@@ -0,0 +1,30 @@
+import {connectToDatabase, getDatabase} from './database/database.js';
+import {typeDefs, resolvers} from './graphql/definations.js';
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = process.env.PORT || 3000
+
+await connectToDatabase();
+
+
+// await updateDB();
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: port },
+    context: async ({ req }) => {
+        return { database: getDatabase() };
+    },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
